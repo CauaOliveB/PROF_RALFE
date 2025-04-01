ff723a3f3e9644782a729ad6cc5e2327acd25181
Cau� Olive Barbosa <52267575817@>
2025-04-01 15:27:08 -0300

    half-compelted content (autentication {zod}, {hookform})

--- enviroment/src/pages/Login.jsx
+++ enviroment/src/pages/Login.jsx
@@ -1,22 +1,56 @@
 import styles from './Login.module.css'
+import {useForm} from 'react-hook-form'
+import {z} from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+
+const loginSchema = z.object({
+    Email: z.string()
+        .email({message: 'Informe um e-mail válido!'}),
+
+    Senha: z.string()
+        .length(6, {message: 'Defina uma senha de 6 caracteres!'})
+})
 
 export function Login(){
+
+    const {register, handleSubmit, formState: {errors}} = useForm({resolver: zodResolver(loginSchema)})
+
+    function autenticarUsuario (data){
+        console.log(data.Email)
+        console.log(data.Senha)
+    }
+
+
     return(
         <div className={styles.container}>
             <p className={styles.titulo}>Login</p>
 
-            <form className={styles.formulario}>
+            <form 
+                onSubmit={handleSubmit(autenticarUsuario)}
+                className={styles.formulario}
+            >
 
                 <input
+                    {...register('Email')}
                     placeholder='E-mail'
                     className={styles.campo} 
                 />
+                { errors.E.mail && (
+                    <p className={estilos.mensagem}>
+                        {errors.Email.message}
+                    </p>
+                )}
 
                 <input
+                    {...register('Senha')}
                     placeholder='Senha'
                     className={styles.campo} 
                 />
-
+                { errors.E.mail && (
+                    <p className={estilos.mensagem}>
+                        {errors.Senha.message}
+                    </p>
+                )}
                 <button
                     className={styles.botao}
                 >Entrar</button>
